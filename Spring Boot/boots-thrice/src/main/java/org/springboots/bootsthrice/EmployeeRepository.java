94726d10fc3789bb7b5d7525c8fc9d09f97810f4
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2026-02-19 10:47:43 +0530

    Delete Spring Boot/boots-thrice directory

--- Spring Boot/boots-thrice/src/main/java/org/springboots/bootsthrice/EmployeeRepository.java
+++
@@ -1,5 +0,0 @@
-package org.springboots.bootsthrice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
